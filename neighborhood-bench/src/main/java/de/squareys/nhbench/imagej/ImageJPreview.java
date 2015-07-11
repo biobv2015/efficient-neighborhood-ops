1a8619fbad2d42b9a210821e4997834de12a37a7
Squareys <Squareys@googlemail.com>
2015-07-11 15:57:56 +0200

    Fix ImageJPreview

--- neighborhood-bench/src/main/java/de/squareys/nhbench/imagej/ImageJPreview.java
+++ neighborhood-bench/src/main/java/de/squareys/nhbench/imagej/ImageJPreview.java
@@ -4,11 +4,17 @@ import java.io.IOException;
 
 import net.imagej.Dataset;
 import net.imagej.ImageJ;
+import net.imagej.ops.Ops;
 import net.imagej.ops.threshold.local.methods.LocalMean;
+import net.imglib2.Cursor;
+import net.imglib2.algorithm.neighborhood.RectangleShape;
 import net.imglib2.exception.IncompatibleTypeException;
 import net.imglib2.img.Img;
+import net.imglib2.img.array.ArrayImgFactory;
+import net.imglib2.outofbounds.OutOfBoundsMirrorFactory;
+import net.imglib2.outofbounds.OutOfBoundsMirrorFactory.Boundary;
+import net.imglib2.type.NativeType;
 import net.imglib2.type.logic.BitType;
-import net.imglib2.type.numeric.RealType;
 import net.imglib2.util.Pair;
 
 /**
@@ -20,27 +26,42 @@ import net.imglib2.util.Pair;
  */
 public class ImageJPreview {
 
-	public static <T extends RealType<T>> void main(String[] args)
+	private static final boolean optimized = false;
+
+	public static <T extends NativeType<T>> void main(String[] args)
 			throws IOException, IncompatibleTypeException {
 		final ImageJ ij = new ImageJ();
 
-		Dataset ds = ij.dataset().open("lena_grey.jpeg");
+		Dataset ds = (Dataset) ij.io().open("lena_grey.jpeg");
 
-		Img<T> in = (Img<T>) ds.getImgPlus().getImg();
+		Img<T> planarin = (Img<T>) ds.getImgPlus().getImg();
+		Img<T> in = new ArrayImgFactory<T>().create(planarin,
+				planarin.firstElement());
+
+		Cursor<T> c1 = planarin.cursor();
+		Cursor<T> c2 = in.cursor();
+
+		while (c1.hasNext()) {
+			c2.next().set(c1.next());
+		}
+
 		Img<BitType> out = in.factory().imgFactory(new BitType())
 				.create(in, new BitType());
+		Img<BitType> outopt = in.factory().imgFactory(new BitType())
+				.create(in, new BitType());
 
-		// ij.op().run(Ops.Threshold.class, out, in,
-		// ij.op().op(LocalMean.class, BitType.class, Pair.class, 0.0),
-		// new RectangleShape(1, false),
-		// new OutOfBoundsMirrorFactory<T, Img<T>>(Boundary.SINGLE));
-
+		ij.op().run(Ops.Threshold.class, out, in,
+				ij.op().op(LocalMean.class, BitType.class, Pair.class, 0.0),
+				new RectangleShape(3, false),
+				new OutOfBoundsMirrorFactory<T, Img<T>>(Boundary.SINGLE));
+		
 		ij.op().run(
 				net.imagej.ops.neighborhood.NeighborhoodWithCenterMap.class,
-				out, in,
-				ij.op().op(LocalMean.class, BitType.class, Pair.class, 0.0), 1);
+				outopt, in,
+				ij.op().op(LocalMean.class, BitType.class, Pair.class, 0.0), 3);
 
-		ij.ui().show("Lena Thersholded", out);
+		ij.ui().show("Lena Thresholded", out);
+		ij.ui().show("Lena Thresholded - optimized", outopt);
 
 	}
 }
